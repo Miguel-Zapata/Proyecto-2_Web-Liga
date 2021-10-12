c57bcf14eb9a9a86ec61b11c120c772d3c3a0e51
Miguel Zapata <miguelzapata.disten@gmail.com>
2021-10-12 10:08:04 +0200

    añadidos fechas y estados

--- scripts/funciones-partidos.js
+++ scripts/funciones-partidos.js
@@ -18,11 +18,17 @@ let encabezado2 = document.createElement('th');
 encabezado2.innerHTML = "Resultados";
 let encabezado3 = document.createElement('th');
 encabezado3.innerHTML = "Equipo Visitante";
+let encabezado4 = document.createElement('th');
+encabezado4.innerHTML = "Fecha";
+let encabezado5 = document.createElement('th');
+encabezado5.innerHTML = "Estado";
 
 /* Asigno los encabezados  como hijos de fila1 y fila1 como hijo de thead. Ambos con appendChild */
 fila1.appendChild(encabezado1);
 fila1.appendChild(encabezado2);
 fila1.appendChild(encabezado3);
+fila1.appendChild(encabezado4);
+fila1.appendChild(encabezado5);
 thead.appendChild(fila1);
 
 for (let i = 0; i < data.matches.length; i++) {
@@ -33,18 +39,25 @@ for (let i = 0; i < data.matches.length; i++) {
     celdaExtra2.innerHTML = `${data.matches[i].score.fullTime.homeTeam} - ${data.matches[i].score.fullTime.awayTeam}`;
     let celdaExtra3 = document.createElement('td');
     celdaExtra3.innerHTML = `${data.matches[i].awayTeam.name}`;
+    let celdaExtra4 = document.createElement('td');
+    celdaExtra4.innerHTML = `${data.matches[i].utcDate}`;
+    let celdaExtra5 = document.createElement('td');
+    celdaExtra5.innerHTML = `${data.matches[i].status}`;
     filaExtra.appendChild(celdaExtra1);
     filaExtra.appendChild(celdaExtra2);
     filaExtra.appendChild(celdaExtra3);
+    filaExtra.appendChild(celdaExtra4);
+    filaExtra.appendChild(celdaExtra5);
     tbody.appendChild(filaExtra);
 }
 
-/* function partidos() {
-    for (let i = 0; i < data.matches.length; i++) {
-        console.log(data.matches[i].homeTeam.name);
-        console.log(data.matches[i].score.fullTime.homeTeam);
-        console.log(data.matches[i].awayTeam.name);
-        console.log(data.matches[i].score.fullTime.awayTeam);
-    }
+function partidos() {
+    /*  for (let i = 0; i < data.matches.length; i++) {
+         console.log(data.matches[i].homeTeam.name);
+         console.log(data.matches[i].score.fullTime.homeTeam);
+         console.log(data.matches[i].awayTeam.name);
+         console.log(data.matches[i].score.fullTime.awayTeam);
+     } */
+    console.log(data);
 }
-partidos(); */
+partidos();
