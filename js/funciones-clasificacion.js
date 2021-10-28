ff24bd89e477e8ef71c1c047b5d79b5b00a657e0
Miguel Zapata <miguelzapata.disten@gmail.com>
2021-10-28 09:31:32 +0200

    comentarios eliminados

--- js/funciones-clasificacion.js
+++ js/funciones-clasificacion.js
@@ -22,34 +22,25 @@ fetch(url, {
 function init(data) {
     crearTablaClasificaciones(data.standings[0].table);
 
-    // PARA QUE EL loader DESAPAREZCA CUANDO CARGA LOS DATOS.
     let loader = document.getElementById('loader');
     loader.classList.add('d-none');
 }
 
 // CREA LA TABLA DE CLASIFICACIÓN
 function crearTablaClasificaciones(clasificaciones) {
-    // COJO LA id DEL tbody PARA INDEXARLO MÁS ADELANTE.
     let tbody = document.getElementById('tbody');
     tbody.setAttribute("class", "tbody_clasificacion");
 
-    // ITERO POR LOS EQUIPOS Y CREO UNA FILA
     for (let i = 0; i < clasificaciones.length; i++) {
         let positionRow = document.createElement('tr');
-
-        // CREO UN ARRAY CON LOS DATOS QUE NECESITO MOSTRAR EN LA TABLA
         let array = [clasificaciones[i].position, `<img src="${clasificaciones[i].team.crestUrl}"/>`, clasificaciones[i].team.name, clasificaciones[i].playedGames, clasificaciones[i].won, clasificaciones[i].draw, clasificaciones[i].lost, clasificaciones[i].goalsFor, clasificaciones[i].goalsAgainst, clasificaciones[i].goalDifference, clasificaciones[i].points, clasificaciones[i].form];
 
-        //  Por cada fila creo celdas nuevas. ¿cuantas? Tantas como clasificaciones tenga array
-        // CREO 1 CELDA POR CADA ELEMENTO DEL ARRAY Y LE AGREGO EL CONTENIDO QUE CORRESPONDE.
         for (let j = 0; j < array.length; j++) {
             let celda = document.createElement('td');
             celda.innerHTML = array[j];
-            // INDEXO LAS CELDAS A LAS FILAS EN EL BUCLE QUE CREA LAS CELDAS.
+
             positionRow.appendChild(celda);
         }
-
-        // INDEXO LAS FILAS AL tbody EN EL BUCLE QUE CREA LAS FILAS.
         tbody.appendChild(positionRow);
     }
 }
--- js/funciones-partidos.js
+++ js/funciones-partidos.js
@@ -25,21 +25,17 @@ function init(data) {
     filter(data.matches);
     console.log(data);
 
-    // PARA QUE EL loader DESAPAREZCA CUABNDO CARGA LOS DATOS.
+
     let loader = document.getElementById('loader');
     loader.classList.add('d-none');
 }
 
 // CREA LA TABLA CON TODOS LOS PARTIDOS.
 function crearTablaPartidos(partidos) {
-
-    // OBTENGO LA id DEL tbody PARA PODER INDEXARLO MÁS ADELANTE.
-    // LE AÑADO UNA CLASE PARA HACER MODIFICACIONES EN CSS.
     let tbody = document.getElementById('tbody');
     tbody.setAttribute("class", "tbody_partidos");
     tbody.innerHTML = "";
 
-    // HAGO UN LOOP POR data.matches PARA CREAR LA TABLA Y AÑADIRLE CONTENIDO.
     for (let i = 0; i < partidos.length; i++) {
         let imagenHome = "<img src='https://crests.football-data.org/" + partidos[i].homeTeam.id + ".svg'/>";
         let imagenAway = "<img src='https://crests.football-data.org/" + partidos[i].awayTeam.id + ".svg'/>";
@@ -49,24 +45,17 @@ function crearTablaPartidos(partidos) {
         celdaExtra1.innerHTML = partidos[i].homeTeam.name;
         let celdaEscudo1 = document.createElement('td');
         celdaEscudo1.innerHTML = imagenHome;
-
-        // EN ESTA CELDA USO UN if PARA CAMBIAR LOS null QUE APARECEN EN LOS PARTIDOS SIN JUGAR Y POSTPUESTOS.
         let celdaExtra2 = document.createElement('td');
         celdaExtra2.innerHTML = `${partidos[i].score.fullTime.homeTeam} - ${partidos[i].score.fullTime.awayTeam}`;
         if (partidos[i].score.fullTime.homeTeam == null && partidos[i].score.fullTime.awayTeam == null) {
             celdaExtra2.innerHTML = 'sin jugar';
         }
-
         let celdaEscudo2 = document.createElement('td');
         celdaEscudo2.innerHTML = imagenAway;
         let celdaExtra3 = document.createElement('td');
         celdaExtra3.innerHTML = partidos[i].awayTeam.name;
-
-        // AQUI USO substring() PARA RECORTAR LA string DE LA FECHA PORQUE APARECEN MUCHOS DATOS.
         let celdaExtra4 = document.createElement('td');
         celdaExtra4.innerHTML = partidos[i].utcDate.substring(0, 10);
-
-        // AQUI USO if Y else if PARA CAMBIAR LOS VALORES POR DEFECTO DE status POR OTROS MÁS ENTENDIBLES.
         let celdaExtra5 = document.createElement('td');
         celdaExtra5.innerHTML = partidos[i].status;
         if (partidos[i].status == "FINISHED") {
@@ -81,7 +70,6 @@ function crearTablaPartidos(partidos) {
             celdaExtra5.innerHTML = "EN JUEGO";
         }
 
-        // INDEXO LAS CELDAS A LA FILA Y ESTA AL tbody PARA GENERAR LA ESTRUCTURA HTML DE LA TABLA.
         filaExtra.appendChild(celdaExtra1);
         filaExtra.appendChild(celdaEscudo1);
         filaExtra.appendChild(celdaExtra2);
@@ -95,59 +83,47 @@ function crearTablaPartidos(partidos) {
 
 // AÑADE LAS OPCIONES A LOS INPUTS TIPO SELECT QUE HARÁN DE FILTROS EN LA PAGINA partidos.html.
 function addOptions(partidos) {
-    // HAGO UN .map POR data.matches PARA OBTENER UN ARRAY CON LOS NOMBRES DE TODOS LOS EQUIPOS.
     let equipos = partidos.map(function(equipo, index, array) {
         return equipo.homeTeam.name;
     });
-    //CON .sort ORDENO ALFABETICAMENTE LOS NOMBRES DE LOS EQUIPOS.
+
     equipos.sort((a, b) => {
-        // .toLowerCase ME SIRVE PARA QUE NO TENGA EN CUENTA LAS MAYUSCULAS A LA HORA DE ORDENAR Y NO LO ORDENE POR SEPARADO.
         a = a.toLowerCase();
         b = b.toLowerCase();
-        // EL ELEMENTO a IRÁ ANTES QUE EL b.
         if (a < b) {
             return -1;
         }
-        // EL ELEMENTO a IRÁ DESPUÉS QUE EL b.
         if (a > b) {
             return 1;
         }
-        //LOS ELEMENTOS MANTIENEN SU POSICIÓN.
         return 0;
     });
 
-    // QUITO TODOS LOS EQUIPOS REPETIDOS CON new Set().
     equipos = new Set(equipos);
-    // AUNQUE EL Set ME DA UN ARRAY, NO ES ITERABLE. ASÍ QUE LO CONVIERTO EN ARRAY CON Array.from().
     equipos = Array.from(equipos);
 
-    //COJO EL PRIMER INPUT CON LA id Y LO GUARDO EN UNA VARIABLE.
     let inputTeam = document.getElementById('team');
-    // RECORRO EL ARRAY DE EQUIPOS QUE OBTUVE CON Array.from() Y AÑADO LOS NOMBRES COMO OPCIONES DEL SELECT.
+
     for (i = 0; i < equipos.length; i++) {
         let option = new Option(equipos[i], equipos[i]); // *(text, value).
-        // INDEXO LAS OPCIONES AL INPUT.
         inputTeam.append(option);
     }
-    // PREPARO EL INPUT CON EL EVENTO change PARA QUE ME DE LA FUNCIÓN filter CUANDO SE PRODUZCA.
+
     inputTeam.addEventListener("change", function() {
         filter(partidos);
     })
 
-    // COJO EL 2º INPUT CON SU id Y LO GUARDO EN UNA VARIABLE.
+
     let inputResult = document.getElementById('result');
-    // CREO MANUALMENTE EL ARRAY CON EL NOMBRE DE LAS OPCIONES QUE CONTENDRÁ EL 2º INPUT.
     let optionResult = ["EMPATA", "GANA", "PIERDE", "PROXIMOS"];
-    // RECORRO EL ARRAY DE RESULTADOS Y LOS AÑADO COMO OPCIONES DEL SELECT.
+
     for (k = 0; k < optionResult.length; k++) {
-        let option = document.createElement("option"); // he probado una sintaxis distinta que en la linea 124 para saber hacerlo de las 2 maneras.
+        let option = document.createElement("option");
         option.value = optionResult[k];
         option.text = optionResult[k];
 
-        // INDEXO LAS OPCIONES AL INPUT.
         inputResult.appendChild(option);
     }
-    // PREPARO EL INPUT PARA EL EVENTO change CON LA FUNCIÓN filter().
     inputResult.addEventListener("change", function() {
         filter(partidos);
     })
@@ -155,17 +131,14 @@ function addOptions(partidos) {
 
 // FILTRA LOS PARTIDOS (data.matches) POR NOMBRE DE EQUIPO Y POR RESULTADOS.
 function filter(partidos) {
-    // COJO LAS ALERTAS CON LA id Y LE AÑADO LA CLASE d-none PARA OCULTARLAS POR DEFECTO.
     let alert = document.getElementById('alert');
     alert.classList.add('d-none');
     let alert2 = document.getElementById('alert2');
     alert2.classList.add('d-none');
 
-    // COJO LOS 2 INPUTS CON SU id.
     let inputTeam = document.getElementById('team');
     let inputResult = document.getElementById('result');
 
-    // LE DIGO QUE SI NO SE HA ESCOGIDO EQUIPO RETIRE LA CLASE d-none DE LA ALERTA, ENTONCES SE MUESTRA.
     if (inputTeam.value == "" && inputResult.value != "") {
         alert.classList.remove('d-none');
     }
@@ -175,10 +148,7 @@ function filter(partidos) {
         return
     }
 
-    // HAGO UN partidos.filter PARA QUE FILTRE EL NOMBRE DE EQUIPO COINCIDIENDO CON EL VALOR (.value) DEL INPUT CORRESPONDIENTE.
-    // .filter ME DEVUELVE UN ARRAY QUE GUARDO EN LA VARIABLE equipo.
     let equipo = partidos.filter(function(partido, i, array) {
-        // EN LOS if TENGO QUE ESPECIFICAR QUE QUIERO QUE ME DEVUELVA true EN LAS COMPARACIONES
         if (partido.awayTeam.name == inputTeam.value) {
             return true;
         }
@@ -188,56 +158,42 @@ function filter(partidos) {
         return false;
     })
 
-    // SI NO ESCOGEMOS NINGÚN RESULTADO MUESTRA UNA TABLA CON TODOS LOS PARTIDOS DEL EQUIPO QUE TENEMOS SELECCIONADO EN EL INPUT 1.
     if (inputResult.value == "") {
         creaTablaFiltrada(equipo);
-        return // este return es para que deje de leer el codigo despues de esta condición.
+        return
     }
 
-    // HAGO UN .filter DEL PRIMER .filter. ASÍ ESTOY FILTRANDO POR RESULTADO DENTRO DEL NOMBRE DE EQUIPO QUE TENGA SELECCIONADO EN EL INPUT 1.
     let results = equipo.filter(function(partido) {
-        // ESPECIFICO CADA COMPARACIÓN CON return true SEGÚN ME INTERSA PARA QUE APAREZCAN SOLO LOS PARTIDOS CUYO 
-        // RESULTADO COINCIDA CON LO SELECCIONADO EN EL INPUT 2.
         if (partido.score.winner == "DRAW" && inputResult.value == "EMPATA") {
-            return true; //empate
+            return true;
         } else if (partido.score.winner == null && inputResult.value == "PROXIMOS") {
-            return true; //proximos
+            return true;
         } else if ((partido.homeTeam.name == inputTeam.value && partido.score.fullTime.homeTeam > partido.score.fullTime.awayTeam) && inputResult.value == "GANA") {
-            return true; //gana
+            return true;
         } else if ((partido.awayTeam.name == inputTeam.value && partido.score.fullTime.awayTeam > partido.score.fullTime.homeTeam) && inputResult.value == "GANA") {
-            return true; // gana
+            return true;
         } else if ((partido.homeTeam.name == inputTeam.value && partido.score.fullTime.homeTeam < partido.score.fullTime.awayTeam) && inputResult.value == "PIERDE") {
-            return true; //pierde
+            return true;
         } else if ((partido.awayTeam.name == inputTeam.value && partido.score.fullTime.awayTeam < partido.score.fullTime.homeTeam) && inputResult.value == "PIERDE") {
-            return true; // pierde
+            return true;
         } else {
             return false;
         }
-
     })
-
-    // CON UN EQUIPO Y UN RESULTADO SELECCIONADO MUETSRA UNA TABLA CON EL ARRAY QUE TENEMOS GUARDADO EN results.
     creaTablaFiltrada(results);
 }
 
 // CREA UNA TABLA CON LOS FILTROS QUE SELECCIONE EN LOS INPUTS.
 function creaTablaFiltrada(partido) {
     let alert2 = document.getElementById('alert2');
-    // COJO EL INPUT DE EQUIPOS POR SU id PARA PODER USARLO EN ESTA FUNCIÓN.
     let inputTeam = document.getElementById('team');
-    // COJO EL tbody POR SU id PARA PODER USARLO EN ESTA FUNCIÓN.
     let tbody = document.getElementById('tbody');
-    // HAGO QUE EL tbody ESTÉ VACÍO PARA PODER CREAR UNA TABLA EN EL.
     tbody.innerHTML = "";
 
-    // LE DIGO QUE SI EL ARRAY QUE ME LLEGA CON LOS FILTROS SELECCIONADOS EN LOS INPUTS ESTÁ VACÍO LE QUITE LA CLASE d-none A
-    // LA ALERTA 2. ASÍ SE MUESTRA.
     if (partido.length == 0 && inputTeam.value !== "") {
         alert2.classList.remove('d-none');
     }
 
-    // RECORRO EL ARRAY QUE ME LLEGA FILTRADO POR LOS INPUTS Y CREO UNA TABLA CON EL CONTENIDO CORRESPONDIENTE.
-    // BASICAMENTE ES IDENTICA A LA PRIMERA TABLA, PERO SOLO MUESTRA EL CONTENIDO CON EL EQUIPO Y EL RESULTADO QUE HAYAMOS SELECCIONADO.
     for (j = 0; j < partido.length; j++) {
         let imagenHome = "<img src='https://crests.football-data.org/" + partido[j].homeTeam.id + ".svg'/>";
         let imagenAway = "<img src='https://crests.football-data.org/" + partido[j].awayTeam.id + ".svg'/>";
